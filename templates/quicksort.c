968fa88f3b5d257d16324338a29a2e9a3a9c728d
Cody Beck <cjb873@nau.edu>
2022-10-04 09:59:37 -0700

    Fix bugs

--- templates/quicksort.c
+++ templates/quicksort.c
@@ -25,7 +25,7 @@ int main()
    srand( SEED );
    int index, iteration;
 
-   for( iteratiom = 0; iteration < ITERATIONS; iteration++ ) 
+   for( iteration = 0; iteration < ITERATIONS; iteration++ ) 
       {
       for( index = 0; index < DATA_SIZE; index++)
          {
@@ -52,7 +52,7 @@ int main()
    sendStringToUart( "Done\n\rArray:\n\r", UART );
 
    
-   for( index = 0; i < DATA_SIZE; index++ ) 
+   for( index = 0; index < DATA_SIZE; index++ ) 
       {
       getStr( array[ index ], intAsStr );
       sendStringToUart( intAsStr, UART );
